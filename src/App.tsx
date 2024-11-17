71c85bdc50e68c68403b388d0084669abb5ce7e1
harkirat <harkirat96@gmail.com>
2024-11-17 21:07:36 +0530

    Added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Button } from './components/Button'
+import { PlusIcon } from './icons/PlusIcon'
 
 function App() {
 
   return (
     <>
-      <Button />
+      <Button startIcon={<PlusIcon />} size="sm" variant="primary" text="Share" />
+      <Button size="md" variant="secondary" text="Add Content" />
+      <Button size="lg" variant="secondary" text="Add Content" />
     </>
   )
 }
